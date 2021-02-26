5394f2b27db38c46137717e1a4ad6264b0c4a11b
arnaldqueiroga <arnaldqueiroga@gmail.com>
2021-02-26 14:25:05 -0300

    07 Praticando herança e interfaces - Implementando tributaveis

---
+++ bytebank-herdado-conta/src/SeguroDeVida.java
@@ -0,0 +1,8 @@
+
+public class SeguroDeVida implements Tributavel{
+	
+	public double getValorImposto() {
+		return 42;
+	}
+
+}
--- bytebank-herdado/src/TesteGerente.java
+++ bytebank-herdado/src/TesteGerente.java
@@ -3,7 +3,7 @@ public class TesteGerente {
 
 	public static void main(String[] args) {
 		
-		Autenticavel referencia = new Gerente();
+		// Autenticavel referencia = new Gerente();
 		
 		Gerente g1 = new Gerente();
 		
